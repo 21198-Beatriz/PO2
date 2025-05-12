0286c60efb60fc315a85e19c7d45abfb3f2ac0f4
24105aprosa <24105@stu.ipbeja.pt>
2025-05-12 15:46:31 +0100

    Create Game.java

---
+++ PO2-Projeto/src/main/java/pt/ipbeja/estig/po2/snowman/model/Game.java
@@ -0,0 +1,11 @@
+package pt.ipbeja.estig.po2.snowman.model;
+
+public class Game {
+    //private LevelManager levelManager;
+    //private Player player;
+    //private MoveStack moveStack;
+
+    public Game() {
+        //...
+    }
+}
